a20a905a7191d88cd0d2ba21070ebf3cfd95deae
juandeaglio <45726108+juandeaglio@users.noreply.github.com>
2021-04-06 10:44:47 -0700

    Initial project setup

---
+++ src/test/SolutionUnitTests.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SolutionUnitTests
+{
+    @Test
+    public void Test()
+    {
+        Assertions.assertTrue(true);
+    }
+}
